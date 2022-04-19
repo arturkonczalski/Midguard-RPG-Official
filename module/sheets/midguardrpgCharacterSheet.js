032b82585ebe16478f87fc5001bf7727f9f86a27
arturkonczalski <67600219+arturkonczalski@users.noreply.github.com>
2022-04-19 13:48:51 +0200

    further character sheet

--- module/sheets/midguardrpgCharacterSheet.js
+++ module/sheets/midguardrpgCharacterSheet.js
@@ -23,3 +23,15 @@ export default class midguardrpgCharacterSheet extends ActorSheet {
     return data;
   }
 }
+let myLabels = document.querySelectorAll(".lbl-toggle");
+
+Array.from(myLabels).forEach((label) => {
+  label.addEventListener("keydown", (e) => {
+    // 32 === spacebar
+    // 13 === enter
+    if (e.which === 32 || e.which === 13) {
+      e.preventDefault();
+      label.click();
+    }
+  });
+});
